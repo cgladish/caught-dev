0709f267d8c200b83fae24679e681591e15f72ea
Chase Gladish <chase.gladish@gmail.com>
2022-09-17 17:14:52 -0700

    Open login with discord window

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const isDev = require("electron-is-dev");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   if (isDev) {
     win.loadURL("http://localhost:3000");
     win.webContents.openDevTools({ mode: "detach" });
   } else {
-    win.loadFile("index.html");
+    win.loadFile("public/index.html");
   }
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    return {
+      action: "allow",
+      overrideBrowserWindowOptions: { autoHideMenuBar: true },
+    };
+  });
 };
 app.whenReady().then(() => {
   createWindow();
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          onClick={() => {
+            window.open(
+              "https://discord.com/login",
+              "_blank",
+              "width=1000,height=600"
+            );
+          }}
         >
-          Learn React
-        </a>
+          Log in with Discord
+        </Button>
       </header>
     </div>
   );
