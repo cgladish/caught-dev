112e4e8a316372478f74596f40e917fa2fb73d83
Chase Gladish <chase.gladish@gmail.com>
2022-09-17 21:30:01 -0700

    Discord auth

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require("electron");
+const { session } = require("electron");
 const isDev = require("electron-is-dev");
+
+let discordAuthorization = null;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -17,12 +20,42 @@ const createWindow = () => {
     win.loadFile("public/index.html");
   }
 
-  win.webContents.setWindowOpenHandler(({ url }) => {
+  win.webContents.setWindowOpenHandler(() => {
     return {
       action: "allow",
       overrideBrowserWindowOptions: { autoHideMenuBar: true },
     };
   });
+  session.defaultSession.webRequest.onBeforeSendHeaders(
+    {
+      urls: ["*://discord.com/*/users/@me/*"],
+    },
+    (details) => {
+      discordAuthorization = details.requestHeaders.Authorization;
+      if (discordAuthorization) {
+        BrowserWindow.getAllWindows().forEach((window) => {
+          if (window.webContents.getURL().includes("discord")) {
+            window.close();
+          }
+        });
+      }
+    }
+  );
+  /*
+  win.webContents.on("did-create-window", (subWindow, details) => {
+    if (details.url.includes("discord")) {
+      subWindow.webContents.debugger.attach("1.1");
+      subWindow.webContents.debugger.on("message", (event, method, params) => {
+        if (method === "Network.responseReceived") {
+          if (params.response.url.includes("/users/@me")) {
+            console.log(params);
+          }
+        }
+      });
+      subWindow.webContents.debugger.sendCommand("Network.enable");
+    }
+  });
+  */
 };
 app.whenReady().then(() => {
   createWindow();
