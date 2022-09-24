cf937cd91e8bc188e3aeb6f6b90d3f8481a6d4e5
Chase Gladish <chase.gladish@gmail.com>
2022-09-24 10:32:47 -0700

    Fix cancel button

--- src/views/Discord/CreateOrEdit/index.tsx
+++ src/views/Discord/CreateOrEdit/index.tsx
@@ -235,7 +235,7 @@ export default function CreateOrEdit() {
       >
         <Button
           startIcon={<Clear />}
-          onClick={() => {}}
+          onClick={() => navigate("..")}
           size="large"
           disabled={isSaving}
         >
